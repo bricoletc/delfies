0ac5c670c5432796db0fdae54754bc0d06d6fda1
Brice Letcher <brice.letcher@ens-lyon.fr>
2024-02-26 15:42:48 +0100

    Row-based approach to breakpoint foci (1)
    * Refactor column-based approach

--- delfies/breakpoint_foci.py
+++ delfies/breakpoint_foci.py
@@ -1,20 +1,15 @@
-from typing import Dict
+from typing import Dict, Set
 import re
 from tempfile import NamedTemporaryFile
 
-from datasci import Tents
-from pysam import AlignmentFile
+from datasci import Tent, Tents
+from pysam import AlignmentFile, AlignedSegment
 
-from delfies.seq_utils import (
-    ID_DELIM,
-    Orientation,
-    ORIENTATIONS,
-    REGION_DELIM1,
-    REGION_DELIM2,
-)
+from delfies.seq_utils import ID_DELIM, Orientation, ORIENTATIONS, parse_region_string
 from delfies.SAM_utils import FLAGS, SoftclippedRead, find_softclip_at_extremity
+from delfies.num_utils import get_contiguous_ranges
 
-FEATURES = ["telo_containing_softclips" + ID_DELIM + o for o in ORIENTATIONS]
+TELO_FEATURES = ["telo_containing_softclips" + ID_DELIM + o for o in ORIENTATIONS]
 READ_FILTER_OUT = (
     FLAGS["UNMAP"] | FLAGS["SECONDARY"] | FLAGS["DUP"] | FLAGS["SUPPLEMENTARY"]
 )
@@ -40,7 +35,7 @@ def find_breakpoint_foci(
     telo_array_size: int,
     cov_window_size: int,
     seq_region: str = None,
-):
+) -> None:
     """
     A note on how the search works:
         - We extract positions in the reference at which telomere-containing soft-clipped reads occur, plus
@@ -50,30 +45,35 @@ def find_breakpoint_foci(
         - Reads are only processed once
         - Statistics are committed as early as possible - saves memory
     An note on **when** we commit statistics:
-        - If softclips occur in reverse orientation, they will occur before the position at which reads containing them are processed -
-          because we only process reads once, using the 'is_head' property, which excludes soft/hard clips
-        - Therefore we must not commit the stats on a position before such reads are processed. Thus we must at least not commit the
-          previous position from the one we are currently processing. In other words we can only commit a position after the position
-          after it is committed.
+        - We could commit a position as soon as it is processed (5'->3'), but this would only work
+          for softclipped reads in the forward orientation, as a softclip-containing position
+          will have been recorded before we process it.
+        - For softclips occurring in reverse orientation, this strategy fails, as they occur
+          before the position at which reads containing them are processed.
+          To deal with this, we commit a position only after it has been processed. This way,
+          when sofclips occur upstream (5') of a processed read, they can only get committed
+          after the condition 'is_head' for the read containing them holds true.
+          (Note: 'is_head' holds true when we are at the left-most mapped base of a read,
+          regardless of its mapped orientation, or the orientation of its softclips).
+        - For that strategy to work, 'cov_window_size' must be > 1.
     """
-    tents_headers = ["contig", "start", "end", "read_depth"] + FEATURES
-    tents = Tents(
-        headers=tents_headers, required_headers=tents_headers[:4], unset_value=0
-    )
-    position_tents: Dict[str, Tent] = {}
+    tents = setup_tents()
+    position_tents: PositionTents = {}
+    positions_to_commit = set()
     pileup_args = dict(
-        flag_filter=READ_FILTER_OUT, min_mapping_quality=MIN_MAPQ, ignore_orphans=False
+        flag_filter=READ_FILTER_OUT,
+        min_mapping_quality=MIN_MAPQ,
+        ignore_orphans=False,
+        truncate=True,
     )
     if seq_region is not None:
-        contig, regs = seq_region.split(REGION_DELIM1)
-        start, stop = map(lambda e: int(e.replace(",", "")), regs.split(REGION_DELIM2))
+        contig, start, stop = parse_region_string(seq_region)
         pileup_args.update(contig=contig, start=start, stop=stop)
     else:
         pileup_args.update(contig=contig_name)
     bam_fstream = AlignmentFile(bam_fname)
     all_pileups = bam_fstream.pileup(**pileup_args)
 
-    positions_to_commit = set()
     for pileup_column in all_pileups:
         ref_name = pileup_column.reference_name
         ref_pos = pileup_column.reference_pos
@@ -96,40 +96,15 @@ def find_breakpoint_foci(
             if not pileup_read.is_head:  # only process a read once
                 continue
             aligned_read = pileup_read.alignment
-            for feature in FEATURES:
-                orientation = Orientation[feature.split(ID_DELIM)[1]]
-                softclipped_read = find_softclip_at_extremity(aligned_read, orientation)
-                if softclipped_read is None:
-                    continue
-                if not has_softclipped_telo_array(
-                    softclipped_read, orientation, telomere_seqs, telo_array_size
-                ):
-                    continue
-                pos_to_commit = softclipped_read.sc_ref
-                match_tent_key = f"{ref_name}{ID_DELIM}{pos_to_commit}"
-                if match_tent_key in position_tents:
-                    position_tents[match_tent_key][feature] += 1
-                else:
-                    new_tent = tents.new()
-                    new_tent.update(
-                        contig=ref_name, start=pos_to_commit, end=pos_to_commit + 1
-                    )
-                    new_tent[feature] += 1
-                    position_tents[match_tent_key] = new_tent
-                positions_to_commit.update(
-                    range(
-                        pos_to_commit - cov_window_size, pos_to_commit + cov_window_size
-                    )
-                )
-        rolling_position = ref_pos - cov_window_size
-        tent_key_to_remove = f"{ref_name}{ID_DELIM}{rolling_position}"
-        if tent_key_to_remove in position_tents:
-            putatively_committed_tent = position_tents.pop(tent_key_to_remove)
-            if rolling_position in positions_to_commit:
-                positions_to_commit.remove(rolling_position)
-                tents.add(putatively_committed_tent)
-    ofpath = NamedTemporaryFile(
-        prefix=f"{ofname_base}_", suffix=".tsv", dir=".", delete=False
-    )
-    with open(ofpath.name, "w") as ofstream:
-        print(tents, file=ofstream)
+            record_softclips(aligned_read, tents, position_tents, positions_to_commit, cov_window_size, telomere_seqs, telo_array_size)
+        putatively_committed_position = ref_pos - cov_window_size
+        putatively_committed_tent_key = (
+            f"{ref_name}{ID_DELIM}{putatively_committed_position}"
+        )
+        if putatively_committed_tent_key not in position_tents:
+            continue
+        putatively_committed_tent = position_tents.pop(putatively_committed_tent_key)
+        if putatively_committed_position in positions_to_commit:
+            positions_to_commit.remove(putatively_committed_position)
+            tents.add(putatively_committed_tent)
+    write_tents(ofname_base, tents)
--- delfies/seq_utils.py
+++ delfies/seq_utils.py
@@ -1,4 +1,5 @@
 from enum import Enum
+from typing import Tuple
 
 ID_DELIM = "__"
 
@@ -35,3 +36,8 @@ REGION_DELIM2 = "-"
 REGION_CLICK_HELP = (
     f"Region to focus on, format: 'contig{REGION_DELIM1}start{REGION_DELIM2}stop'."
 )
+
+def parse_region_string(region_string: str) -> Tuple[str, int, int]:
+    contig, regs = region_string.split(REGION_DELIM1)
+    start, stop = map(lambda e: int(e.replace(",", "")), regs.split(REGION_DELIM2))
+    return contig, start, stop
